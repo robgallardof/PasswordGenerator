e04bbc5030e8a170f12ccd2f2f530e3aeec34e6c
Gallardo <robgallardof@gmail.com>
2024-05-18 03:42:40 -0600

    resize tittle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
   return (
     <Container maxWidth="sm" className="mt-10 bg-black text-white min-h-screen flex flex-col items-center justify-center">
       {/* Page Title with Lock Icon */}
-      <Typography variant="h3" component="h1" gutterBottom className="text-white flex items-center animate-fade-in" style={{ fontFamily: 'Fira Code, monospace' }}>
+      <Typography variant="h4" component="h1" gutterBottom className="text-white flex items-center animate-fade-in" style={{ fontFamily: 'Fira Code, monospace' }}>
         <LockIcon className="mr-2" /> Password Generator
       </Typography>
       {/* Password Generator Component */}
