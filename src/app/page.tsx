2fd153b37db096e941c3cd6c014df3db0f2d88e5
Gallardo <robgallardof@gmail.com>
2024-05-18 03:40:56 -0600

    Rename Tittle Add footer with copyright

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import PasswordGenerator from './components/PasswordGenerator';
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Box, Link } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 
 const Home: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <Container maxWidth="sm" className="mt-10 bg-black text-white min-h-screen flex flex-col items-center justify-center">
       {/* Page Title with Lock Icon */}
@@ -11,6 +14,12 @@ const Home: React.FC = () => {
       </Typography>
       {/* Password Generator Component */}
       <PasswordGenerator />
+      {/* Footer */}
+      <Box mt={4} className="text-center">
+        <Typography variant="body2" className="text-white" style={{ fontFamily: 'Fira Code, monospace' }}>
+          Developed by <Link href="https://RobertoGallardo.dev" target="_blank" rel="noopener noreferrer" className="text-green-500 hover:text-green-400" style={{ fontFamily: 'Fira Code, monospace' }}>Roberto Gallardo</Link> &copy; {currentYear}
+        </Typography>
+      </Box>
     </Container>
   );
 };
